29aa05d360dc5a377f15e3d55d498ed23341dc81
Len Bright <lpb@lowell.edu>
2019-04-03 20:55:55 +0000

    Add files via upload

--- src/camera_ops/proto/asc_scheduler.py
+++ src/camera_ops/proto/asc_scheduler.py
@@ -2,11 +2,13 @@
 from astropy.coordinates import EarthLocation, get_sun, AltAz
 from astropy.time import Time
 import json
+config_file = '/home/sel/indidev/config.json'
 
 class ObsScheduler:
     def __init__(self):
         self.sun_angle = 0
-        with open('config.json') as f:
+        self.sun_deg = 0
+        with open(config_file) as f:
             data = json.load(f)
 
         self.location = EarthLocation.from_geodetic(data['locgeo']['lon'], data['locgeo']['lat'], data['locgeo']['elev'])
@@ -14,8 +16,10 @@ class ObsScheduler:
     def sunang(self):
         altaz = AltAz(location=self.location, obstime=Time.now())
         sun = get_sun(Time.now())
-        self.sun_deg = sun.transform_to(altaz).alt.deg
-
+        # file2read = '/home/sel/indidev/srss'
+        # f = open(file2read, 'r')
+        # self.sun_deg = f.read()
+        self.sun_deg = float(f'{sun.transform_to(altaz).alt.deg:2.2f}')
+        # f.close()
         return self.sun_deg
-
 
